197d0975b0787948e5947de0f660ea1ae5562168
helenerll <heleneruelle@hotmail.com>
2019-08-27 07:37:26 -0700

    270819

--- TestTrashQuest/Controllers/AddQuestViewController.swift
+++ TestTrashQuest/Controllers/AddQuestViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import Firebase
 
 class AddQuestViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     
+    
+    @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var dateTextField: UITextField!
-    @IBOutlet weak var privateLabel: UILabel!
     @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var streetNumberTextField: UITextField!
+    @IBOutlet weak var streetTextField: UITextField!
+    @IBOutlet weak var zipcodeTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var commentTextField: UITextField!
+    @IBOutlet weak var privateLabel: UILabel!
+    @IBOutlet weak var privateSwitch: UISwitch!
     
     private var datePicker: UIDatePicker?
     var type = ["Water", "Land"]
+    var db: Firestore?
+    var refDoc: DocumentReference?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        db = Firestore.firestore()
+        
+        //set switch to unlocked
+        privateSwitch.isOn = false
         
         //setRoundBorder privateLayer
         privateLabel.layer.cornerRadius = 10
@@ -41,6 +57,12 @@ class AddQuestViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         view.addGestureRecognizer(tapGesture)
         dateTextField.inputView = datePicker
 
+    }
+    
+    func getPrivateValue() -> Bool {
+        if privateSwitch.isOn {
+            return true
+        } else {return false}
     }
     
     //dismiss date picker when click elsewhere on screen
@@ -80,5 +102,35 @@ class AddQuestViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
+    
+    
+    @IBAction func addCollectButton(_ sender: Any) {
+        let collectTitle = titleTextField.text
+        let collectType = typeTextField.text
+        let collectDate = dateTextField.text
+        let collectStreetNumber = streetNumberTextField.text
+        let collectStreetName = streetTextField.text
+        let collectZipcode = zipcodeTextField.text
+        let collectCity = cityTextField.text
+        let collectComment = commentTextField.text
+        let collectPrivat = getPrivateValue()
+        if let collectTitle = collectTitle, let collectType = collectType, let collectDate = collectDate, let collectStreetNumber = collectStreetNumber, let collectStreetName = collectStreetName, let collectZipcode = collectZipcode, let collectCity = collectCity {
+            let postData: [String: Any?] = ["title": collectTitle, "type": collectType, "date": collectDate, "streetNumber": collectStreetNumber, "streetName": collectStreetName, "zipcode": collectZipcode, "city": collectCity, "comment": collectComment, "privat": collectPrivat]
+            refDoc = db?.collection("collect").addDocument(data: postData) { (error) in
+                if let error = error {
+                    print("unable to add document to Firestore + \(error)")
+                }
+                else {
+                    print("Document has been added to Firestore + \(self.refDoc?.documentID)")
+                }
+            }
+        }
+        else {
+            Toast.show(message: "Sorry but your quest is missing informations", controller: self)
+        }
+    }
+    
+    
+    
 
 }
